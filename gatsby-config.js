4abd6c272380df3848c09ad957520005f8f08411
David Li <david.li@zepben.com>
2021-07-26 17:41:48 +1000

    Update gatsby-plugin-manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,8 +45,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "Project Tramsformation",
+        short_name: "Tramsformation",
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
