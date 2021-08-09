768f0c6fd4229034fd5a58d3f8e078dc2dfe193d
David Li <david.li@zepben.com>
2021-08-09 16:43:10 +1000

    Update project status counter

--- src/components/ProjectStatus/index.js
+++ src/components/ProjectStatus/index.js
@@ -10,7 +10,7 @@ export default () => (
     <Indicator />
     Project Status:
     <Edition>
-      In Production - 2021 Edition - 18/35 Units Remaining
+      In Production - 2021 Edition - 16/35 Units Remaining
     </Edition>
   </Container>
 );
