1da9f4c37ae631ce63ece2e1ada308660b9a29a8
David Li <fantasticmrdavid@gmail.com>
2023-06-11 22:02:32 +1000

    Upgrade to Gatsby 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,41 +4,39 @@ import "antd/dist/antd.css";
 import Homepage from "./homepage";
 
 // Query for the Blog Home content in Prismic
-export const query = graphql`
-  query MyQuery {
-    prismicBloghome {
-      data {
-        description {
-          text
-        }
-        headline {
-          text
-        }
+export const query = graphql`query MyQuery {
+  prismicBloghome {
+    data {
+      description {
+        text
       }
-      id
-      type
+      headline {
+        text
+      }
     }
-    allPrismicPost(sort: { fields: data___date, order: DESC }) {
-      edges {
-        node {
-          url
-          id
-          uid
-          type
-          data {
-            title {
-              richText
-            }
-            date
-            body {
-              ... on PrismicPostDataBodyText {
-                id
-                slice_label
-                slice_type
-                primary {
-                  text {
-                    richText
-                  }
+    id
+    type
+  }
+  allPrismicPost(sort: {data: {date: DESC}}) {
+    edges {
+      node {
+        url
+        id
+        uid
+        type
+        data {
+          title {
+            richText
+          }
+          date
+          body {
+            ... on PrismicPostDataBodyText {
+              id
+              slice_label
+              slice_type
+              primary {
+                text {
+                  richText
                 }
               }
             }
@@ -47,5 +45,5 @@ export const query = graphql`
       }
     }
   }
-`;
+}`;
 export default withPrismicPreview(Homepage);
--- src/templates/post.js
+++ src/templates/post.js
@@ -3,76 +3,74 @@ import { withPrismicPreview } from "gatsby-plugin-prismic-previews";
 import Post from "components/Post";
 
 // Query for the Blog Post content in Prismic
-export const query = graphql`
-  query BlogPostQuery($uid: String) {
-    site {
-      siteMetadata {
-        title
-        description
+export const query = graphql`query BlogPostQuery($uid: String) {
+  site {
+    siteMetadata {
+      title
+      description
+    }
+  }
+  allPrismicPost(sort: {data: {date: DESC}}) {
+    edges {
+      node {
+        url
+        id
+        uid
+        type
+        data {
+          title {
+            richText
+          }
+        }
       }
     }
-    allPrismicPost(sort: { fields: data___date, order: DESC }) {
-      edges {
-        node {
-          url
+  }
+  prismicPost(uid: {eq: $uid}) {
+    id
+    uid
+    lang
+    type
+    url
+    data {
+      date
+      title {
+        richText
+      }
+      body {
+        ... on PrismicPostDataBodyText {
+          slice_label
+          slice_type
+          primary {
+            text {
+              richText
+            }
+          }
+        }
+        ... on PrismicPostDataBodyQuote {
+          slice_label
+          slice_type
+          primary {
+            quote {
+              richText
+            }
+          }
+        }
+        ... on PrismicPostDataBodyImageWithCaption {
           id
-          uid
-          type
-          data {
-            title {
+          slice_label
+          slice_type
+          primary {
+            image {
+              alt
+              url
+            }
+            caption {
               richText
             }
           }
         }
       }
     }
-    prismicPost(uid: { eq: $uid }) {
-      id
-      uid
-      lang
-      type
-      url
-      data {
-        date
-        title {
-          richText
-        }
-        body {
-          ... on PrismicPostDataBodyText {
-            slice_label
-            slice_type
-            primary {
-              text {
-                richText
-              }
-            }
-          }
-          ... on PrismicPostDataBodyQuote {
-            slice_label
-            slice_type
-            primary {
-              quote {
-                richText
-              }
-            }
-          }
-          ... on PrismicPostDataBodyImageWithCaption {
-            id
-            slice_label
-            slice_type
-            primary {
-              image {
-                alt
-                url
-              }
-              caption {
-                richText
-              }
-            }
-          }
-        }
-      }
-    }
   }
-`;
+}`;
 export default withPrismicPreview(Post);
